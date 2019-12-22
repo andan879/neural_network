bc164caf06f2bc0a8fc04c87136c22adc45f859e
andan879 <andan879@student.liu.se>
2019-12-22 16:01:04 +0100

    added methods: sigmoid and feed_forward

--- deep_learning.py
+++ deep_learning.py
@@ -23,10 +23,33 @@ class NeuralNetwork(object):
 		self.weights = [np.random.randn(y, x)
 						for x, y in zip(sizes[:-1], sizes[1:])]
 
-if True:
-	nn = NeuralNetwork([2, 3, 1])
-	for lay_nr, bias in enumerate(nn.biases):
-		print('biases in layer ', lay_nr + 2, ': \n', bias)
+		def sigmoid(self, z):
+			return 1 / (1 + np.exp(-z))
 
-	for lay_nr, weight in enumerate(nn.weights):
-		print('weights in layer ', lay_nr + 2, ': \n', weight)
+		def feed_forward(self, a):
+			for b, w in zip(self.biases, self.weights):
+				a = sigmoid(np.dot(w, a) + b)
+			return a
+
+# Log some NN properties
+if True:
+	divider = '------------------------------------------------------'
+	nn = NeuralNetwork([2, 3, 1])
+
+	print('###################### Neural Network - Pretty Print ############################')
+	print('Layer design: \n' + str(nn.sizes))
+
+	print(divider)
+
+	for i, bias in enumerate(nn.biases):
+		print"Bias(es) in layer nr {0}: \n".format(i+2), str(bias)
+
+	print(divider)
+ 
+	for i, weight in enumerate(nn.weights):
+		print "Weight(s) connecting layer {0} with layer {1}: \n".format(i, i+1), str(weight)
+
+	print(divider)
+
+	print zip(nn.biases, nn.weights)
+
