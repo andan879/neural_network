5813fa8f9840e98a0699f7e1ac72453fcb9834c4
andan879 <andan879@student.liu.se>
2019-12-24 15:59:35 +0100

    Implemented stochastic gradient descent and a function tu update mini batches.

--- deep_learning.py
+++ deep_learning.py
@@ -31,6 +31,78 @@ class NeuralNetwork(object):
 				a = sigmoid(np.dot(w, a) + b)
 			return a
 
+		def SGD(self, training_data, epochs, mini_batch_size, eta, test_data = None):
+			'''
+			Stochastic Gradient Descent.
+			--------------------------------------------------------
+			training_data: 
+				list of tuples (x, y) where x is the training input
+				and y is the corresponding desired output
+			--------------------------------------------------------
+			epochs:
+				the number of epochs to train for
+			--------------------------------------------------------
+			mini_batch_size:
+				size of the mini_batch when sampling
+			--------------------------------------------------------
+			eta:
+				the learning rate
+			--------------------------------------------------------
+			test_data:
+				if test_data is supplied, the program will evaluate
+				the network after each epoch and print the partial
+				progress
+			--------------------------------------------------------
+
+			'''
+			if test_data:
+				n_test = len(test_data)
+			for j in range(epochs):
+				random.shuffle(training_data)
+				mini_batches = [training_data[k:k + mini_batch_size] for k in (0, n, mini_batch_size)]
+				 
+				for mini_batch in mini_batches:
+					self.update_mini_batch(mini_batch, eta)
+
+				if test_data:
+					print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test)
+				else:
+					print "Epoch {0} complete".format(j)
+
+		def update_mini_batch(self, mini_batch, eta):
+			'''
+			Updates the weights and biases according to a single
+			iteration of SGD. 
+			--------------------------------------------------------
+			mini_batch:
+				a mini_batch is a list of tuples, (x, y)
+			--------------------------------------------------------
+			eta:
+				the learning rate
+			--------------------------------------------------------  
+			'''
+			nabla_b = [np.zeros(b.shape) for b in self.biases]
+			nable_w = [np.zeros(w.shape) for w in self.weights]
+
+			for x, y in mini_batch:
+				delta_nabla_b, delta_nabla_w = self.backdrop(x, y)
+				nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+				nabla_w = [nw + dnw for nw, dnw in zip(nable_w, delta_nabla_w)]
+
+			self.weights = [w - (eta/len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+
+			self.biases = [b - (eta/len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
+
+		def backdrop(x, y):
+			pass
+
+
+
+
+
+
+
+
 # Log some NN properties
 if True:
 	divider = '------------------------------------------------------'
@@ -41,15 +113,4 @@ if True:
 
 	print(divider)
 
-	for i, bias in enumerate(nn.biases):
-		print"Bias(es) in layer nr {0}: \n".format(i+2), str(bias)
-
-	print(divider)
- 
-	for i, weight in enumerate(nn.weights):
-		print "Weight(s) connecting layer {0} with layer {1}: \n".format(i, i+1), str(weight)
-
-	print(divider)
-
-	print zip(nn.biases, nn.weights)
 
